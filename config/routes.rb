d001fdc9590b556373d8275d999c8fa3d06b770e
ntzwilly <ntzwilly@gmail.com>
2022-02-21 21:23:13 +0200

    Add controller class and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'static#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api, defaults: { format: 'json' } do
+    get 'greeting', to: 'greetings#index'
+  end
 end
